d65b2f350569ad158439ea901c6d97c962f8001b
thecodinglife <ttll442003@gmail.com>
2024-11-07 15:39:10 +0700

    KotlinLayoutActivity

---
+++ ff/app/src/main/java/com/learning/ad/ff/KotlinLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.learning.ad.ff
+
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+
+class KotlinLayoutActivity : AppCompatActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+
+   }
+}

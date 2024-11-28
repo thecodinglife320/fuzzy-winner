2556de1468239508b4d5896df80aade84656101d
thecodinglife <ttll442003@gmail.com>
2024-11-28 21:18:21 +0700

    Sqldemo client

---
+++ SQLDemoClient/app/src/main/java/com/learning/ad/sqldemoclient/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.learning.ad.sqldemoclient.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}

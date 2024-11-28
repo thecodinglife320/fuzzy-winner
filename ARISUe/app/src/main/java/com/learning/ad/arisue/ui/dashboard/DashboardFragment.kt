906936c77fb72f5b54fee1065bf684d51fc94b30
thecodinglife <ttll442003@gmail.com>
2024-11-29 00:34:18 +0700

    initial commit

--- ARISUe/app/src/main/java/com/learning/ad/arisue/ui/dashboard/DashboardFragment.kt
+++ ARISUe/app/src/main/java/com/learning/ad/arisue/ui/dashboard/DashboardFragment.kt
@@ -1,4 +1,4 @@
-package com.learning.ad.sqldemoclient.ui.gallery
+package com.learning.ad.arisue.ui.dashboard
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.learning.ad.sqldemoclient.databinding.FragmentGalleryBinding
+import com.learning.ad.arisue.databinding.FragmentDashboardBinding
 
-class GalleryFragment : Fragment() {
+class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,13 +22,15 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel = ViewModelProvider(this)[GalleryViewModel::class.java]
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            binding.textGallery.text= it
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
         }
         return root
     }
--- ARISUe/app/src/main/java/com/learning/ad/arisue/ui/home/HomeFragment.kt
+++ ARISUe/app/src/main/java/com/learning/ad/arisue/ui/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.learning.ad.sqldemoclient.ui.gallery
+package com.learning.ad.arisue.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.learning.ad.sqldemoclient.databinding.FragmentGalleryBinding
+import com.learning.ad.arisue.databinding.FragmentHomeBinding
 
-class GalleryFragment : Fragment() {
+class HomeFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,13 +22,15 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel = ViewModelProvider(this)[GalleryViewModel::class.java]
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            binding.textGallery.text= it
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
         }
         return root
     }
